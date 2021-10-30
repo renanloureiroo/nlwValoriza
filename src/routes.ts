0fbe5400de70b1a51f0d9093fdcfd3a191f6175f
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-10-29 23:52:34 -0300

    Compliment Controller/ route

--- src/routes.ts
+++ src/routes.ts
@@ -5,13 +5,16 @@ import { CreateTagController } from "./controllers/CreateTagController"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 
 import { ensureAdmin } from "./Middlewares/ensureAdmin"
+import { CreateComplimentController } from "./controllers/CreateComplimentController"
 
 const router = Router()
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
+const createComplimentController = new CreateComplimentController()
 const authenticateUserController = new AuthenticateUserController()
 
 router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/compliment", createComplimentController.handle)
 router.post("/users", createUserController.handle)
 router.post("/login", authenticateUserController.handle)
 export { router }
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import "reflect-metadata"
 import express, { Request, Response, NextFunction } from "express"
 import "express-async-errors"
 
-import { router } from "./router"
+import { router } from "./routes"
 
 import "./database"
 
