88d0abbd7bebdc87fad8e9720c71e83445fbff7d
Renan Pereira Loureiro <renanpl47@gmail.com>
2021-11-01 00:37:58 -0300

    compliments Controller user_id do request

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -3,14 +3,14 @@ import { CreateComplimentsService } from "../services/CreateComplimentsService"
 
 class CreateComplimentController {
   async handle(request: Request, response: Response) {
-    const { user_sender, user_receiver, tag_id, message } = request.body
-
+    const { user_receiver, tag_id, message } = request.body
+    const { user_id } = request
     const createCompliment = new CreateComplimentsService()
 
     const compliment = await createCompliment.execute({
-      user_sender,
-      user_receiver,
       tag_id,
+      user_sender: user_id,
+      user_receiver,
       message,
     })
 
